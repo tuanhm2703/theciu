eccf17be5b1ef3022868895589b0fc89c607bebb
loveminh1259 <tuanhm2703@gmail.com>
2023-01-17 10:21:16 +0700

    UPDATE CATEGORY SEEDER

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -16,73 +16,120 @@ class ProductCategorySeeder extends Seeder
      */
     public function run()
     {
-        Category::firstOrCreate([
-            'name' => 'Áo khoác',
+        $shirtCat = Category::firstOrCreate([
+            'name' => 'Áo',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Áo kiểu',
-            'type' => CategoryType::PRODUCT
-        ]);
-        Category::firstOrCreate([
-            'name' => 'Áo len',
-            'type' => CategoryType::PRODUCT
-        ]);
-        Category::firstOrCreate([
+        $shirtCat->categories()->firstOrCreate([
             'name' => 'Áo thun',
-            'type' => CategoryType::PRODUCT
-        ]);
-        Category::firstOrCreate([
-            'name' => 'Sweater',
             'type' => CategoryType::PRODUCT,
-            'parent_id' => Category::whereName('Áo thun')->whereType(CategoryType::PRODUCT)->first()->id
         ]);
-        Category::firstOrCreate([
+        $shirtCat->categories()->firstOrCreate([
             'name' => 'Sơ mi',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Áo vest',
+        $shirtCat->categories()->firstOrCreate([
+            'name' => 'Áo kiểu',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Chân váy',
+        $shirtCat->categories()->firstOrCreate([
+            'name' => 'Áo hai dây và ba lỗ',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Đầm',
+        $shirtCat->categories()->firstOrCreate([
+            'name' => 'Áo polo',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Hàng lỗi',
+        $shirtCat->categories()->firstOrCreate([
+            'name' => 'Áo len',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Jumpsuit',
+        $shirtCat->categories()->firstOrCreate([
+            'name' => 'Khác',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Phụ kiện',
+        $pantCat = Category::firstOrCreate([
+            'name' => 'Quần',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
+        $pantCat->categories()->firstOrCreate([
             'name' => 'Quần dài',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
+        $pantCat->categories()->firstOrCreate([
             'name' => 'Quần jean',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
+        $pantCat->categories()->firstOrCreate([
             'name' => 'Quần short',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
-            'name' => 'Set đồ',
+        $skirtCat = Category::firstOrCreate([
+            'name' => 'Chân váy',
             'type' => CategoryType::PRODUCT
         ]);
-        Category::firstOrCreate([
+        $skirtCat->categories()->firstOrCreate([
+            'name' => 'Chân váy ngắn',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $skirtCat->categories()->firstOrCreate([
+            'name' => 'Chân váy midi',
+             'type' => CategoryType::PRODUCT
+        ]);
+        $dressCat = Category::firstOrCreate([
+            'name' => 'Đầm',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $dressCat->categories()->firstOrCreate([
+            'name' => 'Đầm ngắn',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $dressCat->categories()->firstOrCreate([
+            'name' => 'Đầm maxi',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $coatCat = Category::firstOrCreate([
+            'name' => 'Áo khoác',
+            'type' => CategoryType::PRODUCT,
+        ]);
+        $coatCat->categories()->firstOrCreate([
+            'name' => 'Áo khoác dây kéo',
+            'type' => CategoryType::PRODUCT,
+        ]);
+        $coatCat->categories()->firstOrCreate([
+            'name' => 'Hoodie',
+            'type' => CategoryType::PRODUCT,
+        ]);
+        $coatCat->categories()->firstOrCreate([
+            'name' => 'Sweater',
+            'type' => CategoryType::PRODUCT,
+        ]);
+        $coatCat->categories()->firstOrCreate([
+            'name' => 'Áo vest & blazer',
+            'type' => CategoryType::PRODUCT,
+        ]);
+        $setCat = Category::firstOrCreate([
+            'name' => 'Bộ',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $setCat->categories()->firstOrCreate([
+            'name' => 'Set bộ',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $setCat->categories()->firstOrCreate([
+            'name' => 'Jumpsuit',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $bibsCat = Category::firstOrCreate([
             'name' => 'Yếm',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $bibsCat->categories()->firstOrCreate([
+            'name' => 'Yếm quần',
+            'type' => CategoryType::PRODUCT
+        ]);
+        $bibsCat->categories()->firstOrCreate([
+            'name' => 'Yếm váy',
             'type' => CategoryType::PRODUCT
         ]);
     }
