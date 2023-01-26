b7b8014661471c6f736316cbe14f702d51a07763
loveminh1259 <tuanhm2703@gmail.com>
2023-01-26 18:29:13 +0700

    Update ProductPickItemComponent.php

--- app/Http/Livewire/ProductPickItemComponent.php
+++ app/Http/Livewire/ProductPickItemComponent.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 class ProductPickItemComponent extends Component {
+    public $product;
     public function render() {
         return view('livewire.product-pick-item-component');
     }
