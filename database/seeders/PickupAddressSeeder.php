272fdec91f8077a287a36f3efbf125659dde5826
loveminh1259 <tuanhm2703@gmail.com>
2023-01-26 22:22:46 +0700

    Update PickupAddressSeeder.php

--- database/seeders/PickupAddressSeeder.php
+++ database/seeders/PickupAddressSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\Config;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,10 +15,63 @@ class PickupAddressSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        DB::statement("INSERT INTO `argon`.`addresses` (`id`, `addressable_type`, `addressable_id`, `type`, `details`, `province_id`, `district_id`, `ward_id`, `full_address`, `fullname`, `phone`, `created_at`, `updated_at`, `featured`, `deleted_at`) VALUES (52, 'App\\Models\\Config', 1, 'pickup', '285/24 Đường Cách Mạng Tháng 8', 1, 10, 146, '285/24 Đường Cách Mạng Tháng 8, Phường 12, Quận 10, Thành phố Hồ Chí Minh', 'THE CIU KHO ONLINE', '0775665912', '2023-01-26 22:09:48', '2023-01-26 22:09:48', 0, NULL);
-        INSERT INTO `argon`.`addresses` (`id`, `addressable_type`, `addressable_id`, `type`, `details`, `province_id`, `district_id`, `ward_id`, `full_address`, `fullname`, `phone`, `created_at`, `updated_at`, `featured`, `deleted_at`) VALUES (51, 'App\\Models\\Config', 1, 'pickup', '73 Đường Nguyễn Văn Bảo', 1, 3, 32, '73 Đường Nguyễn Văn Bảo, Phường 4, Quận Gò Vấp, Thành phố Hồ Chí Minh', 'THE CIU Nguyễn Văn Bảo', '0707987912', '2023-01-26 22:08:48', '2023-01-26 22:08:48', 0, NULL);
-        INSERT INTO `argon`.`addresses` (`id`, `addressable_type`, `addressable_id`, `type`, `details`, `province_id`, `district_id`, `ward_id`, `full_address`, `fullname`, `phone`, `created_at`, `updated_at`, `featured`, `deleted_at`) VALUES (50, 'App\\Models\\Config', 1, 'pickup', '50 Tô Vĩnh Diện', 1, 8, 104, '50 Tô Vĩnh Diện, Phường Linh Chiểu, Thành phố Thủ Đức, Thành phố Hồ Chí Minh', 'THE CIU Tô Vĩnh Diện', '0333707912', '2023-01-26 22:08:16', '2023-01-26 22:08:16', 0, NULL);
-        INSERT INTO `argon`.`addresses` (`id`, `addressable_type`, `addressable_id`, `type`, `details`, `province_id`, `district_id`, `ward_id`, `full_address`, `fullname`, `phone`, `created_at`, `updated_at`, `featured`, `deleted_at`) VALUES (49, 'App\\Models\\Config', 1, 'pickup', '680 Sư Vạn Hạnh', 1, 10, 148, '680 Sư Vạn Hạnh, Phường 10, Quận 10, Thành phố Hồ Chí Minh', 'THE CIU Sư Vạn Hạnh', '0707358912', '2023-01-26 22:04:13', '2023-01-26 22:04:13', 0, NULL);
-        INSERT INTO `argon`.`addresses` (`id`, `addressable_type`, `addressable_id`, `type`, `details`, `province_id`, `district_id`, `ward_id`, `full_address`, `fullname`, `phone`, `created_at`, `updated_at`, `featured`, `deleted_at`) VALUES (48, 'App\\Models\\Config', 1, 'pickup', '29/0 Đường Nguyễn Gia Trí', 1, 4, 43, '29/0 Đường Nguyễn Gia Trí, Phường 25, Quận Bình Thạnh, Thành phố Hồ Chí Minh', 'THE CIU D2', '0901246912', '2023-01-26 21:50:21', '2023-01-26 21:50:21', 1, NULL);");
+        $config = Config::firstOrCreate();
+        $config->pickup_addresses()->create([
+            'type' =>'pickup',
+            'details'=> '285/24 Đường Cách Mạng Tháng 8',
+            'province_id' => 1,
+            'district_id' => 10,
+            'ward_id' => 146,
+            'full_address' => '285/24 Đường Cách Mạng Tháng 8, Phường 12, Quận 10, Thành phố Hồ Chí Minh',
+            'fullname' => 'THE CIU KHO ONLINE',
+            'phone' => '0775665912',
+            'featured' => 1
+        ]);
+        $config->pickup_addresses()->create([
+            'type' =>'pickup',
+            'details'=> '73 Đường Nguyễn Văn Bảo',
+            'province_id' => 1,
+            'district_id' => 3,
+            'ward_id' => 32,
+            'full_address' => '73 Đường Nguyễn Văn Bảo, Phường 4, Quận Gò Vấp, Thành phố Hồ Chí Minh',
+            'fullname' => 'THE CIU Nguyễn Văn Bảo',
+            'phone' => '0707987912',
+            'featured' => 0
+        ]);
+        $config->pickup_addresses()->create([
+            'type' =>'pickup',
+            'details'=> '50 Tô Vĩnh Diện',
+            'province_id' => 1,
+            'district_id' => 8,
+            'ward_id' => 104,
+            'full_address' => '50 Tô Vĩnh Diện, Phường Linh Chiểu, Thành phố Thủ Đức, Thành phố Hồ Chí Minh',
+            'fullname' => 'THE CIU Tô Vĩnh Diện',
+            'phone' => '0775665912',
+            'featured' => 0
+        ]);
+
+
+        $config->pickup_addresses()->create([
+            'type' =>'pickup',
+            'details'=> '29/0 Đường Nguyễn Gia Trí',
+            'province_id' => 1,
+            'district_id' => 10,
+            'ward_id' => 146,
+            'full_address' => '285/24 Đường Cách Mạng Tháng 8, Phường 12, Quận 10, Thành phố Hồ Chí Minh',
+            'fullname' => 'THE CIU KHO ONLINE',
+            'phone' => '0775665912',
+            'featured' => 0
+        ]);
+        $config->pickup_addresses()->create([
+            'type' =>'pickup',
+            'details'=> '680 Sư Vạn Hạnh',
+            'province_id' => 1,
+            'district_id' => 4,
+            'ward_id' => 43,
+            'full_address' => '29/0 Đường Nguyễn Gia Trí, Phường 25, Quận Bình Thạnh, Thành phố Hồ Chí Minh',
+            'fullname' => 'THE CIU D2',
+            'phone' => '0901246912',
+            'featured' => 0
+        ]);
     }
 }
