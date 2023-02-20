7bcf48ecc9b7ae9e6e49d86c753cd427f5294483
loveminh1259 <tuanhm2703@gmail.com>
2023-02-20 09:46:32 +0700

    update payment amount

--- resources/views/livewire/client/order-details.blade.php
+++ resources/views/livewire/client/order-details.blade.php
@@ -3,7 +3,7 @@
         <div class="order-header d-flex justify-content-between bg-light p-3">
             <span class="text-uppercase w-25">
                 < Trở lại</span>
-                    <div class="w-100">
+                    <div class="w-100 text-right">
                         <span>Mã đơn hàng. {{ $order->order_number }}</span> | <span
                             class="text-danger">{{ $order->getCurrentStatusLabel() }}</span>
                     </div>
