6baeca077196a39386ca37853a727bfc8d67e2d5
loveminh1259 <tuanhm2703@gmail.com>
2023-02-28 21:11:30 +0700

    fix bug ui

--- resources/views/landingpage/layouts/components/navs/promotion.blade.php
+++ resources/views/landingpage/layouts/components/navs/promotion.blade.php
@@ -3,7 +3,7 @@
 
     <div class="megamenu megamenu-sm">
         <div class="row no-gutters">
-            <div class="col-md-6">
+            <div class="col-12">
                 <div class="menu-col">
                     <ul>
                         @foreach ($promotions as $p)
@@ -16,29 +16,6 @@
                         @endforeach
                     </ul>
                 </div><!-- End .menu-col -->
-            </div><!-- End .col-md-6 -->
-
-            <div class="col-md-6">
-                <div class="banner banner-overlay owl-carousel owl-theme owl-nav-inside owl-light" data-toggle="owl"
-                    data-owl-options='{
-                    "nav": false,
-                    "dots": false,
-                    "autoplay": true,
-                    "autoplayTimeout": 3000
-                }'>
-                    @foreach ($promotions as $promotion)
-                        @foreach ($promotion->products as $p)
-                            <a href="{{ route('client.product.details', $p->slug) }}">
-                                <img src="{{ optional($p->image)->path_with_domain }}" alt="Banner">
-
-                                <div class="banner-content banner-content-bottom">
-                                    <div class="banner-title text-white">{{ $p->name }}</div>
-                                    <!-- End .banner-title -->
-                                </div><!-- End .banner-content -->
-                            </a>
-                        @endforeach
-                    @endforeach
-                </div><!-- End .banner -->
             </div><!-- End .col-md-6 -->
         </div><!-- End .row -->
     </div><!-- End .megamenu megamenu-sm -->
