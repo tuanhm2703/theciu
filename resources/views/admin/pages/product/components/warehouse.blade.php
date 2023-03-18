de2410168d97c94b727c9bb79d75e17ca6e0d6af
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-03-18 17:32:16 +0700

    Update warehouse.blade.php

--- resources/views/admin/pages/product/components/warehouse.blade.php
+++ resources/views/admin/pages/product/components/warehouse.blade.php
@@ -1,3 +1,4 @@
 @foreach ($product->inventories as $inventory)
-    <p class="text-sm font-weight-bold mb-0 text-center">{{ $inventory->stock_quantity }}</p>
+    <p class="text-sm font-weight-bold mb-0 text-center {{ $inventory->stock_quantity == 0 ? 'text-danger' : '' }}">
+        {{ $inventory->stock_quantity == 0 ? 'Hết hàng' : $inventory->stock_quantity }}</p>
 @endforeach
