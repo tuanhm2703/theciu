4e56f4205bc17977a7e42482fda1b7b84329fba5
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-03-18 14:49:53 +0700

    Update sku.blade.php

--- resources/views/admin/pages/product/components/sku.blade.php
+++ resources/views/admin/pages/product/components/sku.blade.php
@@ -1,3 +1,3 @@
 @foreach ($product->inventories as $inventory)
-    <p class="text-sm font-weight-bold mb-0">{{ $inventory->sku }} <small class="text-danger">{{ $inventory->active ? '' : '(Chưa đồng bộ Kiot)' }}</small></p>
+    <p class="text-sm font-weight-bold mb-0">{{ $inventory->sku }} <small class="text-danger">{{ $inventory->status ? '' : '(Chưa đồng bộ Kiot)' }}</small></p>
 @endforeach
