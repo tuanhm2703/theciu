ff8686984714d0a6e93098ade13d5e802a98110d
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-03-23 15:14:59 +0700

    add google tag

--- resources/views/landingpage/layouts/app.blade.php
+++ resources/views/landingpage/layouts/app.blade.php
@@ -31,16 +31,6 @@
     @livewireStyles
 </head>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BZXGHRRRX3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BZXGHRRRX3');
-</script>
-
 <body>
     <div class="page-wrapper">
 
--- resources/views/landingpage/layouts/meta.blade.php
+++ resources/views/landingpage/layouts/meta.blade.php
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BZXGHRRRX3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BZXGHRRRX3');
+</script>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
