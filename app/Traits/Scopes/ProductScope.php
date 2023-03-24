d0124b75820ca2c2552a0456a03d4f140b76322f
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-03-24 20:41:24 +0700

    Update ProductScope.php

--- app/Traits/Scopes/ProductScope.php
+++ app/Traits/Scopes/ProductScope.php
@@ -55,7 +55,7 @@ trait ProductScope
 
     public function scopeWithNeededProductCardData($q)
     {
-        return $q->available()->with('image:path,imageable_id,id,name', 'available_flash_sales', 'inventories.image', 'categories', 'inventories')->select('id', 'slug', 'name');
+        return $q->available()->with('image:path,imageable_id,id,name', 'available_flash_sales', 'inventories.image', 'categories', 'inventories')->select('products.id', 'products.slug', 'products.name');
     }
 
     public function scopeAddSalePrice($q)
