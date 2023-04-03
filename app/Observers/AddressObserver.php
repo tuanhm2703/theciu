179f7e3ec04e4d2d0984a6dd10f769936b20dbb9
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-03 14:43:41 +0700

    Update AddressObserver.php

--- app/Observers/AddressObserver.php
+++ app/Observers/AddressObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Address;
+use App\Models\Ward;
 
 class AddressObserver {
     public function creating(Address $address) {
@@ -10,6 +11,7 @@ class AddressObserver {
     }
 
     public function updating(Address $address) {
-        $address->full_address = $address->details . ", " . $address->ward->path_with_type;
+        $ward = Ward::find($address->ward_id);
+        $address->full_address = $address->details . ", " . $ward->path_with_type;
     }
 }
