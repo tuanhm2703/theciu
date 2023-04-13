7e625d146f9dd42b684d46f6b2dd0fcb2cccd79e
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-13 19:47:29 +0700

    fix cors error

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://beta.theciu.vn',
+        'https://theciu.vn',
+    ],
 
     'allowed_origins_patterns' => [],
 
