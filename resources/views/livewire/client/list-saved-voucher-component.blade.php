81efc9a17e9642b5553b4bf7634984516dd7542c
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-15 16:41:26 +0700

    update list saved voucher

--- resources/views/livewire/client/list-saved-voucher-component.blade.php
+++ resources/views/livewire/client/list-saved-voucher-component.blade.php
@@ -1,4 +1,4 @@
-<div id="save-voucher-component-wrapper" class="{{ $show ? '' : 'hide' }}" wire:init="loadContent">
+<div id="save-voucher-component-wrapper" class="{{ $show ? '' : 'hide' }}" wire:init="loadvouchers">
     @if ($readyToLoad)
         <i class="fa fa-angle-double-down" id="close-voucher-list-btn" wire:click="$set('show', {{ !$show }})"></i>
         @if ($vouchers->count() == 0)
@@ -13,6 +13,7 @@
 </div>
 <script>
     document.addEventListener("DOMContentLoaded", () => {
+
         @this.updateVoucherStatus();
     })
 </script>
