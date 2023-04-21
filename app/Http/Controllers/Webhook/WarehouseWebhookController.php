fbd5780f2598132b770eb4a1e2c72cf8102c9b80
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-21 13:22:44 +0700

    Update WarehouseWebhookController.php

--- app/Http/Controllers/Webhook/WarehouseWebhookController.php
+++ app/Http/Controllers/Webhook/WarehouseWebhookController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers\Webhook;
 
 use App\Http\Controllers\Controller;
 use App\Models\Inventory;
+use App\Models\KiotProduct;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
-class WarehouseWebhookController extends Controller {
-    public function updateKiotProduct(Request $request) {
+class WarehouseWebhookController extends Controller
+{
+    public function updateKiotProduct(Request $request)
+    {
         try {
             $data = $request->Notifications[0]['Data'][0];
             $sku = $data['ProductCode'];
             $inventory = Inventory::whereSku($sku)->firstOrFail();
             $kiotConfig = App::get('KiotConfig');
-            if($inventory->sku == $sku && $kiotConfig->data['branchId'] == $data['BranchId']) {
-                $inventory->stock_quantity = $data['OnHand'] - $data['Reversed'];
+            if ($inventory->sku == $sku && $kiotConfig->data['branchId'] == $data['BranchId']) {
+                $inventory->stock_quantity = $data['OnHand'] - $data['Reserved'];
                 $inventory->stock_quantity = $inventory->stock_quantity < 0 ? 0 : $inventory->stock_quantity;
                 $inventory->status = $data['isActive'];
                 $inventory->save();
