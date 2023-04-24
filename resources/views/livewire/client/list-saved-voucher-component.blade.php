a3d8876d28cc69e0b370a20341ed43c5d9353225
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-24 12:36:09 +0700

    Update list-saved-voucher-component.blade.php

--- resources/views/livewire/client/list-saved-voucher-component.blade.php
+++ resources/views/livewire/client/list-saved-voucher-component.blade.php
@@ -1,6 +1,6 @@
 <div id="save-voucher-component-wrapper" class="hide" wire:init="loadVouchers">
     @if ($readyToLoad)
-        <i class="fa fa-angle-double-down" id="close-voucher-list-btn"></i>
+        <i class="fa fa-angle-double-down w-100 text-center" id="close-voucher-list-btn"></i>
         @if ($vouchers->count() == 0)
             <div class="text-center">
                 <i>{{ trans('labels.no_voucher_available') }}</i>
