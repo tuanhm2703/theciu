cccb3a20f840384b3a8b2b56c7eee0e79ad42352
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-26 12:01:24 +0700

    update voucher icon

--- app/Http/Livewire/Client/VoucherPopupComponent.php
+++ app/Http/Livewire/Client/VoucherPopupComponent.php
@@ -24,7 +24,7 @@ class VoucherPopupComponent extends Component
 
     public function loadVouchers() {
         if(customer()) {
-            $this->vouchers = Voucher::notExpired()->where('quantity', '>', 0)->saveable()->get();
+            $this->vouchers = Voucher::notExpired()->where('quantity', '>', 0)->with('voucher_type')->saveable()->get();
             $saved_vouchers = customer()->saved_vouchers()->get();
             $this->vouchers->each(function($voucher) use ($saved_vouchers) {
                 $voucher->saved = in_array($voucher->id, $saved_vouchers->pluck('id')->toArray());
@@ -34,7 +34,7 @@ class VoucherPopupComponent extends Component
                 return $value->used == false;
             });
         } else {
-            $this->vouchers = Voucher::notExpired()->where('quantity', '>', 0)->featured()->saveable()->get();
+            $this->vouchers = Voucher::notExpired()->where('quantity', '>', 0)->with('voucher_type')->featured()->saveable()->get();
         }
         $this->readyToLoad = true;
         if(!Session::has('prevent-reopen-voucher-popup') && $this->vouchers->where('saved', 0)->where('quantity', '>', 0)->count() > 0) {
