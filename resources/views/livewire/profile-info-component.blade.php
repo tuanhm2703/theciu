21dd855a3a4f913e1204bf4c512232ef45a27c5a
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-04-30 15:41:26 +0700

    Update profile-info-component.blade.php

--- resources/views/livewire/profile-info-component.blade.php
+++ resources/views/livewire/profile-info-component.blade.php
@@ -5,64 +5,69 @@
                 {{ Session::get('message') }}
             </div>
         @endif
-            {!! Form::model($user, []) !!}
-            <div class="row">
-                <div class="col-sm-6">
-                    <label>{{ trans('labels.first_name') }} *</label>
-                    {!! Form::text('first_name', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.first_name']) !!}
-                    @error('user.first_name')
-                        <span class="error">{{ $message }}</span>
-                    @enderror
-                </div><!-- End .col-sm-6 -->
+        {!! Form::model($user, []) !!}
+        <div class="row">
+            <div class="col-sm-6">
+                <label>{{ trans('labels.first_name') }} *</label>
+                {!! Form::text('first_name', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.first_name']) !!}
+                @error('user.first_name')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div><!-- End .col-sm-6 -->
 
-                <div class="col-sm-6">
-                    <label>{{ trans('labels.last_name') }} *</label>
-                    {!! Form::text('last_name', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.last_name']) !!}
-                </div><!-- End .col-sm-6 -->
-            </div><!-- End .row -->
+            <div class="col-sm-6">
+                <label>{{ trans('labels.last_name') }} *</label>
+                {!! Form::text('last_name', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.last_name']) !!}
+            </div><!-- End .col-sm-6 -->
+        </div><!-- End .row -->
 
-            <div class="row">
-                <div class="col-12 col-lg-6">
-                    <label>{{ trans('labels.email_address') }} *</label>
-                    {!! Form::text('email', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.email']) !!}
-                    @error('user.email')
-                        <span class="error">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="col-12 col-lg-6">
-                    <label>{{ trans('labels.phone') }} *</label>
-                    {!! Form::text('phone', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.phone']) !!}
-                    @error('user.phone')
-                        <span class="error">{{ $message }}</span>
-                    @enderror
-                </div>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <label>{{ trans('labels.email_address') }} *</label>
+                {!! Form::text('email', null, ['class' => 'form-control', 'required', 'wire:model' => 'user.email']) !!}
+                @error('user.email')
+                    <span class="error">{{ $message }}</span>
+                @enderror
             </div>
-            @if (empty(customer()->provider))
-                <div class="row">
-                    <div class="col-12 col-lg-6">
-                        {!! Form::label('password', trans('labels.password'), []) !!}
-                        <div class="position-relative">
-                            <input type="password" class="form-control" readonly name="password" disabled
-                                value="********">
-                            <a class="d-flex align-items-center" href="#" wire:click="showPasswordForm"
-                                style="position: absolute;
+            <div class="col-12 col-lg-6">
+                <label>{{ trans('labels.phone') }} *</label>
+                <div class="position-relative">
+                    <span>{{ $user->phone }}</span>
+                    <a class="d-flex align-items-center" href="{{ route('client.auth.profile.phone') }}"
+                        style="position: absolute;
                                top: 50%;
                                right: 0;
                                transform: translate(-10%, -50%);}">
-                                <div wire:loading wire:target="showPasswordForm" class="spinner-grow"
-                                    style="width: 3rem; height: 3rem;" role="status">
-                                    <span class="sr-only">Loading...</span>
-                                </div>{{ trans('labels.change_password') }}
-                            </a>
-                        </div>
+                        {{ trans('labels.change_phone') }}
+                    </a>
+                </div>
+                @error('user.phone')
+                    <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        @if (empty(customer()->provider))
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    {!! Form::label('password', trans('labels.password'), []) !!}
+                    <div class="position-relative">
+                        <input type="password" class="form-control" readonly name="password" disabled value="********">
+                        <a class="d-flex align-items-center" href="{{ route('client.auth.profile.password') }}"
+                            style="position: absolute;
+                               top: 50%;
+                               right: 0;
+                               transform: translate(-10%, -50%);}">
+                            {{ trans('labels.change_password') }}
+                        </a>
                     </div>
                 </div>
-            @endif
-            <button type="submit" wire:click.prevent="update" class="btn btn-outline-primary-2">
-                <span wire:loading.remove wire:target="update">{{ trans('labels.update') }} <i
-                        class="icon-long-arrow-right"></i></span>
-                <span wire:loading wire:target="update">Saving..</span>
-            </button>
-            {!! Form::close() !!}
+            </div>
+        @endif
+        <button type="submit" wire:click.prevent="update" class="btn btn-outline-primary-2">
+            <span wire:loading.remove wire:target="update">{{ trans('labels.update') }} <i
+                    class="icon-long-arrow-right"></i></span>
+            <span wire:loading wire:target="update">Saving..</span>
+        </button>
+        {!! Form::close() !!}
     </div>
 </div>
