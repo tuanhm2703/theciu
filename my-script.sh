43715c4577b92051597370b386e6ccf40878aab2
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-05-30 21:55:54 +0700

    update docker-compose

---
+++ my-script.sh
@@ -0,0 +1,7 @@
+name=$(date +"%Y%m%d_%H%M%S")
+filename="backup_$name"
+result=$(gdrive --service-account ../../credentials.json mkdir $filename)
+folderId=$(echo "$result" | awk '{print $2}')
+gdrive --service-account ../../credentials.json upload ../../backup --name /$filename --parent $folderId --recursive
+gdrive --service-account ../../credentials.json share $folderId
+gdrive --service-account ../../credentials.json info $folderId
