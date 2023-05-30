f771b523f0952b1c1eca8da3ed9517277ab037a1
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-05-30 23:18:19 +0700

    update docker backup script

--- my-script.sh
+++ my-script.sh
@@ -2,6 +2,7 @@ name=$(date +"%Y%m%d_%H%M%S")
 filename="backup_$name"
 result=$(gdrive --service-account ../../credentials.json mkdir $filename)
 folderId=$(echo "$result" | awk '{print $2}')
-gdrive --service-account ../../credentials.json upload ../../backup --name /$filename --parent $folderId --recursive
+mysqldump -h mariadb -u thec_theciu -p4OM@e*Ebow-n^jEy thec_theciu > backup.sql
+gdrive --service-account ../../credentials.json upload ../../backup.sql --parent $folderId --recursive
 gdrive --service-account ../../credentials.json share $folderId
 gdrive --service-account ../../credentials.json info $folderId
