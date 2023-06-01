d0990de5cffbf4d9b5e152f1c921fa5453a43b74
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-06-01 13:39:34 +0700

    update credential

--- my-script.sh
+++ my-script.sh
@@ -2,7 +2,7 @@ name=$(date +"%Y%m%d_%H%M%S")
 filename="backup_$name"
 result=$(gdrive --service-account ../../credentials.json mkdir $filename)
 folderId=$(echo "$result" | awk '{print $2}')
-mysqldump -h mariadb -u root -p4OM@e*Ebow-n^jEy theciu-prod > backup.sql
+mysqldump -h mariadb -u root -p4OM@e*Ebow-n^jEy theciu-prod > /backup.sql
 gdrive --service-account ../../credentials.json upload ../../backup.sql --parent $folderId --recursive
 gdrive --service-account ../../credentials.json share $folderId
 gdrive --service-account ../../credentials.json info $folderId
