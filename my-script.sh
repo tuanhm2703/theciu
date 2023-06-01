ff52a89cc62674344305c4e969e849724859bdd3
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-06-01 16:34:07 +0700

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -4,5 +4,5 @@ result=$(gdrive --service-account ../../credentials.json mkdir $filename)
 folderId=$(echo "$result" | awk '{print $2}')
 mysqldump -h mariadb -u root -p4OM@e*Ebow-n^jEy theciu-prod > /backup.sql
 gdrive --service-account ../../credentials.json upload ../../backup.sql --parent $folderId --recursive
-gdrive --service-account ../../credentials.json share $folderId
+gdrive --service-account ../../credentials.json share $folderId --type user --email tuanhm.store@gmail.com
 gdrive --service-account ../../credentials.json info $folderId
