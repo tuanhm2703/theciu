332aefe2b5acd19599ae98c56456e50627ebd64d
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-06-10 16:35:59 +0700

    Update DealOfDayComponent.php

--- app/Http/Livewire/Client/DealOfDayComponent.php
+++ app/Http/Livewire/Client/DealOfDayComponent.php
@@ -12,7 +12,7 @@ class DealOfDayComponent extends Component
 
     public function mount() {
         $this->flash_sale_products = Cache::remember("deal_of_day", env('CACHE_EXPIRE', 600), function() {
-            return Product::whereHas('available_flash_sales')->withNeededProductCardData()->get();
+            return Product::whereHas('flash_sale')->withNeededProductCardData()->get();
         });
     }
 
