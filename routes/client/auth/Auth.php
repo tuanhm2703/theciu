f2d3d5daa171b068dc632a70510f2ac67521c8a7
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-06-19 12:46:55 +0700

    Update Auth.php

--- routes/client/auth/Auth.php
+++ routes/client/auth/Auth.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\Client\Auth\AuthController;
 
-Route::group(['prefix' => 'auth', 'as' => 'auth.', 'middleware' => ['phoneVerification', 'auth:customer']], function () {
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     include('Profile.php');
     include('Cart.php');
