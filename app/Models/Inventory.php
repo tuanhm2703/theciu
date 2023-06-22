5bba9ec3f070ba75a1bdc121ebe664b10668d781
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-06-22 12:02:40 +0700

    Update Inventory.php

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -55,7 +55,7 @@ class Inventory extends Model {
     }
 
     public function product() {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->withTrashed();
     }
 
     public function getFormattedPriceAttribute() {
