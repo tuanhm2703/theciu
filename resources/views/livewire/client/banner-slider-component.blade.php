f28bbfac69751f2b127ad4da39245869ce7fe01b
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-07-06 14:45:40 +0700

    Update banner-slider-component.blade.php

--- resources/views/livewire/client/banner-slider-component.blade.php
+++ resources/views/livewire/client/banner-slider-component.blade.php
@@ -14,7 +14,7 @@
         @foreach ($banners as $banner)
 
             <div class="intro-slide">
-                <img alt="the ciu banner" src="{{ $banner->image->getPathWithSize(1600) }}">
+                <img alt="THE C.I.U BANNER - {{ $banner->title }}" src="{{ $banner->image->getPathWithSize(1600) }}">
                 {{-- <div class="container intro-content text-center">
                     <h3 class="intro-subtitle text-white">{{ $banner->title }}</h3><!-- End .h3 intro-subtitle -->
                     <h1 class="intro-title text-white">{{ $banner->title }}</h1><!-- End .intro-title -->
