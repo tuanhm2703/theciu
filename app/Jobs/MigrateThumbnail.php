7e44eb815ef365af0c9ad84c995a0ce1ddf25df0
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-07-17 16:58:54 +0700

    create migrate thumbnail

---
+++ app/Jobs/MigrateThumbnail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class MigrateThumbnail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public Image $image;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Image $image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->image->migrateThumbnail();
+    }
+}
