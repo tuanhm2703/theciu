13c06c6eef074730880b12a06179e09546e56bc8
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-07-18 14:06:35 +0700

    update review

--- resources/views/landingpage/layouts/pages/product/detail/index.blade.php
+++ resources/views/landingpage/layouts/pages/product/detail/index.blade.php
@@ -129,13 +129,14 @@
                             <div class="row no-gutters">
                                 <div class="col-auto">
                                     <div class="text-center">
-                                        <a class="social-icon" target="_blank" style="overflow: hidden" ><img
+                                        <a class="social-icon" target="_blank" style="overflow: hidden"><img
                                                 src="{{ $review->customer->avatar_path }}" alt=""></a>
                                     </div>
                                     <h4 class="mb-0"><a href="#">{{ $review->customer->full_name }}</a></h4>
                                     <div class="ratings-container">
                                         <div class="ratings">
-                                            <div class="ratings-val" style="width: {{ $review->product_score * 20 }}%;"></div><!-- End .ratings-val -->
+                                            <div class="ratings-val" style="width: {{ $review->product_score * 20 }}%;">
+                                            </div><!-- End .ratings-val -->
                                         </div><!-- End .ratings -->
                                     </div><!-- End .rating-container -->
                                 </div><!-- End .col -->
@@ -147,7 +148,8 @@
                                     <div class="d-flex flex-nowrap">
                                         @if ($review->video)
                                             <div class="p-1">
-                                                <a class="popup-vimeo popup-media" style="background: url({{ $review->video->thumbnail_url }})"
+                                                <a class="popup-vimeo popup-media"
+                                                    style="background: url({{ $review->video->thumbnail_url }})"
                                                     href="{{ $review->video->path_with_domain }}">
                                                     <div class="video-icon-label">
                                                         <i class="far fa-play-circle"></i>
@@ -157,14 +159,24 @@
                                         @endif
                                         @foreach ($review->images as $image)
                                             <div class="p-1">
-                                                <a href="{{ $image->path_with_domain }}" class="img-popup popup-media" style="background: url({{ $image->path_with_domain }})">
+                                                <a href="{{ $image->path_with_domain }}" class="img-popup popup-media"
+                                                    style="background: url({{ $image->path_with_domain }})">
                                                     {{-- <img src="{{ $image->path_with_domain }}" alt=""> --}}
                                                 </a>
                                             </div>
                                         @endforeach
                                     </div>
                                     <div class="review-action">
-                                        <a href="#"><i class="icon-thumbs-up"></i>({{ $review->likes }})</a>
+                                        <a href="#">
+                                            {!! Form::open([
+                                                'url' => route('client.auth.review.like', $review->id),
+                                                'method' => 'PUT',
+                                                'class' => 'review-react-form'
+                                            ]) !!}
+                                                <button type="submit" class="icon-thumbs-up btn btn-link border-0 px-0 text-dark"></button>
+                                            {{ $review->likes ? $review->likes : '' }}
+                                            {!! Form::close() !!}
+                                        </a>
                                     </div><!-- End .review-action -->
                                 </div><!-- End .col-auto -->
                             </div><!-- End .row -->
@@ -192,5 +204,19 @@
                 clickable: true,
             }
         });
+        $('.review-react-form').ajaxForm({
+            beforeSend: function(xhr, options, $i) {
+                console.log(xhr, options, $i);
+                console.log(this);
+            },
+            success: function(response, statusText, xhr, $form) {
+                console.log($form);
+            },
+            error: (err) => {
+                if(err.status === 401) {
+                    openLoginModal()
+                }
+            }
+        })
     </script>
 @endpush
