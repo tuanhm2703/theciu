d92c48668a5d2a54ab51c7177da5999ab30ef428
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-08-05 11:25:11 +0700

    Update app.blade.php

--- resources/views/landingpage/layouts/app.blade.php
+++ resources/views/landingpage/layouts/app.blade.php
@@ -85,7 +85,7 @@
 
 
     <!-- Sign in / Register Modal -->
-    @if (auth('customer')->check())
+    @if (!auth('customer')->check())
         <div class="modal fade" id="signin-modal" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
