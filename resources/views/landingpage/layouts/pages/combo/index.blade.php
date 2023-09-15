d4b31b788e4eb4944e36f20f68c4b91a5b856f41
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-09-15 19:30:14 +0700

    Update index.blade.php

--- resources/views/landingpage/layouts/pages/combo/index.blade.php
+++ resources/views/landingpage/layouts/pages/combo/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <main class="main">
         <div>
-            <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
+            <nav aria-label="breadcrumb" class="breadcrumb-nav">
                 <div class="container">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/">{{ trans('labels.dashboard') }}</a></li>
