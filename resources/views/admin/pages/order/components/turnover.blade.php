dc5a630383602957b5ebce79275ea54bed3b97c9
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-09-22 10:57:33 +0700

    Update turnover.blade.php

--- resources/views/admin/pages/order/components/turnover.blade.php
+++ resources/views/admin/pages/order/components/turnover.blade.php
@@ -1,2 +1,2 @@
-<span>{{ format_currency_with_label($order->subtotal) }}</span><br>
+<span>{{ format_currency_with_label($order->getFinalRevenue()) }}</span><br>
 <p class="text-sm">{{trans('labels.payment_methods.'.$order->payment_method->code)}}</p>
