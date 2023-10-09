a5ee05eac4d889e36810832fd106b97ef8876b6e
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-10-09 10:25:55 +0700

    update image width

--- resources/views/landingpage/layouts/pages/product/index.blade.php
+++ resources/views/landingpage/layouts/pages/product/index.blade.php
@@ -1,4 +1,11 @@
 @extends('landingpage.layouts.app')
+@push('css')
+    <style>
+        .page-content img {
+            width: 100% !important;
+        }
+    </style>
+@endpush
 @section('content')
     <livewire:client.product-list-component :banners="isset($banners) ? $banners : []" :category="$category ?? null" :haspromotion="isset($haspromotion)" :type="isset($type) ? $type : null"
         :title="isset($title) ? $title : null" :promotion="isset($promotion) ? $promotion : null" />
