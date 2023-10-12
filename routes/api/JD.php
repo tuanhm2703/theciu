8f3ddebee634853c13d4c00433beb2dec961b72e
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-10-12 21:49:12 +0700

    add api detail job

--- routes/api/JD.php
+++ routes/api/JD.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\Api\JdController;
 
 Route::group(['prefix' => 'jds'], function() {
     Route::get('', [JdController::class, 'index']);
+    Route::get('/{jd}', [JdController::class, 'detail']);
     Route::post('/{jd}/resumes', [JdController::class, 'postResume']);
 });
