c217719e7538e337549e4bcbc3c07b1dd6cdbb1b
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-11-10 00:24:34 +0700

    Update index.blade.php

--- resources/views/landingpage/layouts/pages/product/index.blade.php
+++ resources/views/landingpage/layouts/pages/product/index.blade.php
@@ -6,7 +6,7 @@
             height: fit-content !important;
         }
         .product-countdown .countdown-show4 .countdown-section {
-            width: calc(25% - 10px);
+            width: calc(25% - 10px) !important;
         }
     </style>
 @endpush
