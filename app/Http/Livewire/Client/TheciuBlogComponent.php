44a9e699d6eeeac6828aa1df9f565c7d936a0ac9
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-11-26 21:41:40 +0700

    Update TheciuBlogComponent.php

--- app/Http/Livewire/Client/TheciuBlogComponent.php
+++ app/Http/Livewire/Client/TheciuBlogComponent.php
@@ -5,32 +5,34 @@ namespace App\Http\Livewire\Client;
 use App\Models\TheciuBlog;
 use Livewire\Component;
 
-class TheciuBlogComponent extends Component
-{
+class TheciuBlogComponent extends Component {
     public $leftBlogs;
     public $rightBlogs;
 
     public function mount() {
-        $this->leftBlogs = TheciuBlog::where('post_type', 'post')
-        ->where('ping_status', 'open')
-        ->whereHas('meta_attachment')
-        ->with('meta_attachment', function($q) {
-            return $q->with('meta_attachment');
-        })
-        ->orderBy('post_date', 'desc')
-        ->limit(5)->get();
-        $this->rightBlogs = TheciuBlog::where('post_type', 'post')
-        ->where('ping_status', 'open')
-        ->whereHas('meta_attachment')
-        ->with('meta_attachment', function($q) {
-            return $q->with('meta_attachment');
-        })
-        ->orderBy('post_date', 'desc')
-        ->whereNotIn('ID', $this->leftBlogs->pluck('ID')->toArray())
-        ->limit(10)->get();
+        try {
+            $this->leftBlogs = TheciuBlog::where('post_type', 'post')
+                ->where('ping_status', 'open')
+                ->whereHas('meta_attachment')
+                ->with('meta_attachment', function ($q) {
+                    return $q->with('meta_attachment');
+                })->orderBy('post_date', 'desc')
+                ->limit(5)->get();
+            $this->rightBlogs = TheciuBlog::where('post_type', 'post')
+                ->where('ping_status', 'open')
+                ->whereHas('meta_attachment')
+                ->with('meta_attachment', function ($q) {
+                    return $q->with('meta_attachment');
+                })
+                ->orderBy('post_date', 'desc')
+                ->whereNotIn('ID', $this->leftBlogs->pluck('ID')->toArray())
+                ->limit(10)->get();
+        } catch (\Throwable $th) {
+            $this->leftBlogs = [];
+            $this->rightBlogs = [];
+        }
     }
-    public function render()
-    {
+    public function render() {
         return view('livewire.client.theciu-blog-component');
     }
 }
