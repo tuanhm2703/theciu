9415870cf2021ec66e740d3950e05c60b584cf84
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-15 00:11:39 +0700

    Update JdObserver.php

--- app/Observers/JdObserver.php
+++ app/Observers/JdObserver.php
@@ -23,13 +23,4 @@ class JdObserver
             $jd->slug = $jd->slug . "-" . $code;
         }
     }
-
-    public function updating(Jd $jd) {
-        $jd->slug = stripVN($jd->title);
-        while (Jd::whereSlug($jd->slug)->exists()) {
-            $jd->slug = stripVN($jd->title);
-            $code = uniqid();
-            $jd->slug = $jd->slug . "-" . $code;
-        }
-    }
 }
