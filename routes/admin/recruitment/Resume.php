01ca5b79c6a74587eda379bf06c148f33c6ea0ae
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-16 01:17:15 +0700

    Update Resume.php

--- routes/admin/recruitment/Resume.php
+++ routes/admin/recruitment/Resume.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\ResumeController;
 
 Route::group(['prefix' => 'resume', 'as' => 'resume.'], function() {
     Route::get('', [ResumeController::class, 'index'])->name('index');
-    Route::get('/{resume}/pdf', [ResumeController::class, 'pdf'])->name('answer');
-    Route::get('/{resume}/answer', [ResumeController::class, 'showAnswer'])->name('pdf');
+    Route::get('/{resume}/pdf', [ResumeController::class, 'pdf'])->name('pdf');
+    Route::get('/{resume}/answer', [ResumeController::class, 'showAnswer'])->name('answer');
     Route::get('/paginate', [ResumeController::class, 'paginate'])->name('paginate');
 });
