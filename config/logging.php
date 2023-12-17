9b8e1db9ff32c73d87c9dc64fad4041f19157a8e
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-17 15:13:31 +0700

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -64,17 +64,21 @@ return [
         ],
         'momo' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/momo/momo.log')
+            'path' => storage_path('logs/momo/momo.log'),
+            'days' => 14,
+            'permission' => 0664,
         ],
         'kiot' => [
             'driver' => 'daily',
             'path' => storage_path('logs/kiot/kiot.log'),
             'days' => 14,
+            'permission' => 0664,
         ],
         'vnpay' => [
             'driver' => 'daily',
             'path' => storage_path('logs/vnpay/vnpay.log'),
             'days' => 14,
+            'permission' => 0664,
         ],
         'daily' => [
             'driver' => 'daily',
