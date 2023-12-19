e3b3dbd35891102d67536eaa078340ec6efb5444
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-19 10:43:06 +0700

    Update WarehouseWebhookController.php

--- app/Http/Controllers/Webhook/WarehouseWebhookController.php
+++ app/Http/Controllers/Webhook/WarehouseWebhookController.php
@@ -8,6 +8,7 @@ use App\Models\KiotProduct;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use VienThuong\KiotVietClient\WebhookType;
 
 class WarehouseWebhookController extends Controller
 {
@@ -15,14 +16,22 @@ class WarehouseWebhookController extends Controller
     {
         try {
             $data = $request->Notifications[0]['Data'][0];
-            $sku = $data['ProductCode'];
-            $inventory = Inventory::whereSku($sku)->firstOrFail();
-            $kiotConfig = App::get('KiotConfig');
-            if ($inventory->sku == $sku && $kiotConfig->data['branchId'] == $data['BranchId']) {
-                $inventory->stock_quantity = $data['OnHand'] - $data['Reserved'];
-                $inventory->stock_quantity = $inventory->stock_quantity < 0 ? 0 : $inventory->stock_quantity;
-                $inventory->status = $data['isActive'];
-                $inventory->save();
+            if(isset($request->Notifications[0]["Action"]) && (strpos($request->Notifications[0]["Action"], WebhookType::PRODUCT_DELETE) > -1)) {
+                $codes = $request->Notifications[0]['Data'];
+                $skus = KiotProduct::whereIn('kiot_product_id', $codes)->pluck('kiot_code')->toArray();
+                Product::whereHas('inventories', function($q) use ($skus){
+                    $q->whereIn('inventories.sku', $skus);
+                })->delete();
+            } else {
+                $sku = $data['ProductCode'];
+                $inventory = Inventory::whereSku($sku)->firstOrFail();
+                $kiotConfig = App::get('KiotConfig');
+                if ($inventory->sku == $sku && $kiotConfig->data['branchId'] == $data['BranchId']) {
+                    $inventory->stock_quantity = $data['OnHand'] - $data['Reserved'];
+                    $inventory->stock_quantity = $inventory->stock_quantity < 0 ? 0 : $inventory->stock_quantity;
+                    $inventory->status = $data['isActive'];
+                    $inventory->save();
+                }
             }
         } catch (\Throwable $th) {
             \Log::error($th->getMessage());
