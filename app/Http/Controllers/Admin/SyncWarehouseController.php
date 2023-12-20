2ef0aa21e8a1d26fd80c9e77ceff18b9133e10f2
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-20 16:18:18 +0700

    Update SyncWarehouseController.php

--- app/Http/Controllers/Admin/SyncWarehouseController.php
+++ app/Http/Controllers/Admin/SyncWarehouseController.php
@@ -81,6 +81,8 @@ class SyncWarehouseController extends Controller {
                 ]);
             }
         }
+        $skus = KiotProduct::pluck('kiot_code')->toArray();
+        Inventory::whereNotIn('sku', $skus)->update(['stock_quantity' => 0]);
     }
 
     public function syncStock(Request $request) {
