9099889290916b692532c45a7024f3ea7ddf8e16
Henry Hoang <62024320+tuanhm2703@users.noreply.github.com>
2023-12-27 09:39:45 +0700

    Update footer-component.blade.php

--- resources/views/livewire/client/footer-component.blade.php
+++ resources/views/livewire/client/footer-component.blade.php
@@ -28,7 +28,7 @@
                         <h4 class="widget-title">Chi nhánh</h4><!-- End .widget-title -->
                         <ul class="widget-list">
                             @foreach ($branches as $branch)
-                                {{ $branch->full_address }} <br>Hotline: {{ $branch->phone }}
+                                {{ $branch->address?->full_address }} <br>Hotline: {{ $branch->phone }}
                             @endforeach
                         </ul>
                     </div><!-- End .widget about-widget -->
