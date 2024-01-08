a130c721f59031a7a43e73c9a465a14c7892a5a7
Henry Hoang <tuanhm2703@gmail.com>
2024-01-08 12:39:08 +0700

    update creator blog

---
+++ app/Http/Resources/Api/BlogResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BlogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $result = parent::toArray($request);
+        $result['created_by'] = $this->creator?->full_name;
+        return $result;
+    }
+}
