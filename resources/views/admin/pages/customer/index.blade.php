2a4255e817796403b8e663ac9301840a65bd757a
Henry Hoang <tuanhm2703@gmail.com>
2024-01-22 13:45:50 +0700

    Update index.blade.php

--- resources/views/admin/pages/customer/index.blade.php
+++ resources/views/admin/pages/customer/index.blade.php
@@ -69,6 +69,8 @@
                 },
                 {
                     data: "orders_count",
+                    searchable: false,
+                    sortable: false
                 },
                 {
                     data: "total_revenue",
