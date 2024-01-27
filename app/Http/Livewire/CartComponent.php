927fe66ebbc3b2069236a0f9d84041005b0d81dd
Henry Hoang <tuanhm2703@gmail.com>
2024-01-27 10:57:30 +0700

    Update CartComponent.php

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -67,7 +67,7 @@ class CartComponent extends Component {
     public $accom_inventory_ids = [];
     public $accom_inventories;
     public $accom_product_promotions;
-    public $accom_product_inventory_ids;
+    public $accom_product_inventory_ids = [];
     public $accom_product_inventories;
     public $accom_product_selected = [];
     protected $rules = [
