4f29001ecd32fdf2325920d5bf42dfe9ff538f52
Henry Hoang <tuanhm2703@gmail.com>
2024-04-20 14:18:51 +0700

    Update footer-component.blade.php

--- resources/views/livewire/client/footer-component.blade.php
+++ resources/views/livewire/client/footer-component.blade.php
@@ -8,7 +8,7 @@
                         <h4 class="widget-title">THE C.I.U TRADING COMPANY LIMITED</h4><!-- End .widget-title -->
                         <ul class="widget-list">
                             <li>
-                                Địa chỉ; 285/24 Cách Mạng Tháng 8, Phường 12, Quận 10, Thành Phố Hồ Chí Minh
+                                Địa chỉ; 64 Tân Canh, Phường 1, Quận Tân Bình, Thành phố Hồ Chí Minh
                             </li>
                             <li>
                                 Mã số thuế: 0317478388
