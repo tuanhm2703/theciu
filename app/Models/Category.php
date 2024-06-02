df1076c16db39ca61b90ed3bbf6d02b2b4896c0d
Henry Hoang <tuanhm2703@gmail.com>
2024-06-02 15:51:33 +0700

    update wishlist list

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\CategoryType;
 use App\Traits\Common\Imageable;
 use App\Traits\Common\Metable;
+use App\Traits\Common\Wishlistable;
 use App\Traits\Scopes\CategoryScope;
 use App\Traits\Scopes\CustomScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,9 +13,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
-class Category extends Model
-{
-    use HasFactory, SoftDeletes, Imageable, CategoryScope, CustomScope, Metable;
+class Category extends Model {
+    use HasFactory, SoftDeletes, Imageable, CategoryScope, CustomScope, Metable, Wishlistable;
     protected $fillable = [
         'name',
         'parent_id',
@@ -53,24 +53,24 @@ class Category extends Model
     public function generateUniqueSlug() {
         $base_slug = stripVN($this->name);
         $slug = $base_slug;
-        while(Category::where('slug', $slug)->where('id', '!=', $this->id)->exists()) {
-            $slug = "$base_slug-".now()->timestamp;
+        while (Category::where('slug', $slug)->where('id', '!=', $this->id)->exists()) {
+            $slug = "$base_slug-" . now()->timestamp;
         }
         return $slug;
     }
     public static function getMenuCategories() {
-        return Category::whereNull('parent_id')->with('categories', function($q) {
-            $q->withCount('products')->with('categories', function($q) {
+        return Category::whereNull('parent_id')->with('categories', function ($q) {
+            $q->withCount('products')->with('categories', function ($q) {
                 $q->withCount('products');
             });
-        })->withCount('products')->where(function($q) {
+        })->withCount('products')->where(function ($q) {
             $q->whereType(CategoryType::PRODUCT)->orWhere('type', CategoryType::COLLECTION);
         })->where('visible', 1)->orderBy('order')->get();
     }
     public function hasProducts() {
-        if($this->products_count == 0) {
-            foreach($this->categories as $c) {
-                if($c->hasProducts()) return true;
+        if ($this->products_count == 0) {
+            foreach ($this->categories as $c) {
+                if ($c->hasProducts()) return true;
             }
         } else {
             return true;
@@ -80,9 +80,9 @@ class Category extends Model
 
     public function getAllChildId() {
         $ids = [$this->id];
-        if($this->categories) {
+        if ($this->categories) {
             foreach ($this->categories as $c) {
-                if($c->id != $this->id) {
+                if ($c->id != $this->id) {
                     $ids = array_merge($ids, $c->getAllChildId());
                 }
             }
@@ -96,7 +96,7 @@ class Category extends Model
         $types = Category::where('type', '!=', CategoryType::PRODUCT)->select('type')->groupBy('type')->get()->pluck('type')->toArray();
         $options = [];
         foreach ($types as $type) {
-            $options[__('labels.'.$type)] = Category::where('type', $type)->pluck('name', 'id')->toArray();
+            $options[__('labels.' . $type)] = Category::where('type', $type)->pluck('name', 'id')->toArray();
         }
         return $options;
     }
