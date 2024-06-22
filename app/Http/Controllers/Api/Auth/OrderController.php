eca436ba45de43f93d23f9d579275e14c2603483
Henry Hoang <tuanhm2703@gmail.com>
2024-06-23 00:33:39 +0700

    update api order

--- app/Http/Controllers/Api/Auth/OrderController.php
+++ app/Http/Controllers/Api/Auth/OrderController.php
@@ -16,15 +16,19 @@ class OrderController extends Controller
         $to = $request->to ? carbon($request->to)->endOfDay() : Order::max('created_at');
         $pageSize = $request->pageSize ?? 10;
         $orderStatus = $request->orderStatus;
-        $orders = $user->orders()->whereBetween('orders.created_at', [$from, $to])->orderBy('created_at', 'desc')->with('inventories.image', 'inventories.attributes');
+        $orders = $user->orders()->whereBetween('orders.created_at', [$from, $to])
+        ->orderBy('created_at', 'desc')
+        ->with('inventories.image', 'inventories.attributes', 'payment_method', 'vouchers', 'order_histories');
         if($orderStatus) $orders->where('order_status', $orderStatus);
         $orders = $orders->paginate($pageSize);
         $paginateData = $orders->toArray();
+        $order_counts = Order::selectRaw('count(id) as order_count, order_status')->groupBy('order_status')->get();
         return BaseResponse::success([
             'items' => OrderListResource::collection($orders),
             'total' => $paginateData['total'],
             'next_page' => $paginateData['next_page_url'],
             'prev_page' => $paginateData['prev_page_url'],
+            'order_counts' => $order_counts
         ]);
     }
 
