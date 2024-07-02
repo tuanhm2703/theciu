8c7040204aa46922e362ee16b6790510f499888c
Henry Hoang <tuanhm2703@gmail.com>
2024-07-02 23:59:43 +0700

    add payment method to config response

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Services\Config\ConfigService;
+use App\Models\PaymentMethod;
 use App\Responses\Api\BaseResponse;
 use Illuminate\Http\Request;
 
@@ -16,9 +17,11 @@ class ConfigController extends Controller
 
     public function getGeneralConfig() {
         $customer_keywords = requestUser() ? explode(',', requestUser()->search_keywords) : [];
+        $payment_methods = PaymentMethod::active()->with('image:imageable_id,path')->get();
         return BaseResponse::success([
             'customer_keywords' => $customer_keywords,
             'trending_keywords' => $this->configservice->getTrendingKeywords(),
+            'payment_methods' => $payment_methods
         ]);
     }
 }
